85f05c2ad505de4990a8f4603b691c77a4740526
Liz4444 <112504549+Liz4444@users.noreply.github.com>
2022-09-13 10:17:34 -0600

    Add files via upload

---
+++ Periodo3- info/Ejercicio 18.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int opcion = 0;
+	do
+    {
+	
+	    cout << "Ingrese 1 para mostrar mensaje\n";
+	    cout << "Ingrese 2 para hacer algo mas\n";
+	    cout << "Ingrese 0 para salir\n";
+	    
+	    cout << "Ingrese una opción a ejecutar\n";
+	    cin >> opcion;
+    }
+    //Mostrar el menú hasta que la opción sea cero
+    while(opcion != 0);
+    system("PAUSE");
+    return 0;
+}
